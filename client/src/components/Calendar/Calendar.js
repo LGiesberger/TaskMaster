7a0a10df47fdb5c90222e6174e6e22c9ee18076f
LGiesberger <lougiesberger@gmail.com>
2021-09-28 15:18:23 +0200

    Calendar improvements

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -1,40 +1,71 @@
-import { getDates, formatMonth } from '../../utils/utils';
+import { getDates, formatMonth, numerifyDate } from '../../utils/utils';
 import rightArrowIcon from '../../images/chevron-right-solid.svg';
 import leftArrowIcon from '../../images/chevron-left-solid.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Calendar.css';
+import { selectDateAction } from '../../redux/actions/dateActions';
+import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 export default function CalendarPage() {
-  const dispatch = useDispatch();
   const date = new Date();
   const [month, setMonth] = useState(date.getMonth());
   const [year, setYear] = useState(date.getFullYear());
-  const [dates, setDates] = useState(
-    month === 11
-      ? getDates(new Date(year, month, 1), new Date(year + 1, 0, 0))
-      : getDates(new Date(year, month, 1), new Date(year, month + 1, 0))
-  );
+  const [dates, setDates] = useState(getDates(month, year));
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   function handleClick(date) {
-    console.log(date);
-    // Button click functionality
+    dispatch(selectDateAction(Number(numerifyDate(date))));
+    history.push('/');
   }
+
+  function handleNext() {
+    if (month === 11) {
+      setYear((prevYear) => prevYear + 1);
+      setMonth(0);
+    } else setMonth((prevMonth) => prevMonth + 1);
+  }
+
+  function handlePrevious() {
+    setMonth((prevMonth) => prevMonth - 1);
+  }
+
+  useEffect(() => {
+    setDates(getDates(month, year));
+  }, [month, year]);
 
   return (
     <div>
       <div className="header">
-        <img src={leftArrowIcon} alt="arrow icon left" />
+        <img
+          src={leftArrowIcon}
+          alt="arrow icon left"
+          onClick={handlePrevious}
+        />
         <h4 className="header-title">{formatMonth(date)}</h4>
-        <img src={rightArrowIcon} alt="arrow icon right" />
+        <img src={rightArrowIcon} alt="arrow icon right" onClick={handleNext} />
       </div>
       <div className="calendar">
+        <div className="weekdays">
+          <p className="weekday">Mon</p>
+          <p className="weekday">Tue</p>
+          <p className="weekday">Wed</p>
+          <p className="weekday">Thu</p>
+          <p className="weekday">Fri</p>
+          <p className="weekday">Sat</p>
+          <p className="weekday">Sun</p>
+        </div>
         <div className="month">
           {dates.map((date) => {
             return (
               <button
                 key={date.getDate()}
-                className="calendar-tile"
+                className={
+                  date.getDate() === new Date().getDate()
+                    ? 'calendar-tile today'
+                    : 'calendar-tile'
+                }
                 onClick={() => handleClick(date)}
               >
                 {date.getDate()}
